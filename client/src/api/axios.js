d679edfec94a8392daf0a332b6c5b7552e7f3bf9
hashimshaji23 <hashimshaji12@gmail.com>
2026-09-16 01:00:47 +0530

    deploy commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://e-commerc.onrender.com", // replace with your actual Render URL
+    baseURL: "https://e-commerc-irrt.onrender.com",
 });
 
 export default API;
